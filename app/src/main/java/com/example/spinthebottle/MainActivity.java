44b1df2b153c0d1fae168cdcc11d17efa5dcdfc0
dineshraj92422 <dineshraj9724226506@gmail.com>
2020-08-15 16:38:59 +0530

    Development completed

--- app/src/main/java/com/example/spinthebottle/MainActivity.java
+++ app/src/main/java/com/example/spinthebottle/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
-    ImageView imgbottle;
+public class MainActivity extends AppCompatActivity {
+    private ImageView imgbottle;
+    private Random random =new Random();
+    private int lastDir;
+    private boolean spinning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
         
     }
 
-    public void spinboottle(View view) {
+    public void spinbottle(View view) {
+        if(!spinning){
+            int newDir = random.nextInt(1800);
+            float pivotX =imgbottle.getWidth()/2;
+            float pivotY =imgbottle.getHeight()/2;
+
+            Animation rotate =new RotateAnimation(lastDir,newDir,pivotX,pivotY);
+            rotate.setDuration(2500);
+            rotate.setFillAfter(true);
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning=true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning=false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            lastDir =newDir;
+            imgbottle.startAnimation(rotate);
+        }
     }
 }
